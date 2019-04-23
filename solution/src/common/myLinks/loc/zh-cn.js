43d0d2a798051c330923cc294d0e6543b5174f41
shrenky <shrenkyang@gmail.com>
2019-04-23 11:23:08 +0800

    Create zh-cn.js

---
+++ solution/src/common/myLinks/loc/zh-cn.js
@@ -0,0 +1,29 @@
+define([], function() {
+  return {
+    "DialogSaveButton": "保存",
+    "DialogCancelButton": "取消",
+    "DialogAddButton": "添加",
+    "DialogUpdateButton": "更新",
+
+    "MyLinksDialogTitle": "我的链接",
+    "MyLinksDialogDescription": "管理我的链接",
+    "SingleLinkDialogTitle": "添加/编辑链接",
+    "SingleLinkDialogDescription": "管理（添加/编辑）一个链接",
+    "ConfirmDeleteLink": "确定删除选择的链接？",
+
+    "LinkTitleLabel": "标题",
+    "LinkUrlLabel": "URL",
+    "InvalidUrlError": "无效的URL",
+
+    "TitleColumn": "标题",
+    "TitleColumnAriaLabel": "URL的标题",
+    "UrlColumn": "URL",
+    "UrlColumnAriaLabel": "URL的链接",
+    "SelectionColumnAriaLabel": "选择",
+    "SelectionAllColumnAriaLabel": "全选",
+    
+    "AddLinkCommand": "添加",
+    "EditLinkCommand": "编辑",
+    "DeleteLinkCommand": "删除"
+    }
+});
